737605ffdb733af7eec545507a5c55f1f8576f2f
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-05-24 12:02:38 -0500

    initialized springboot file and set up auth and server

---
+++ src/main/java/com/example/javastarting2/JavaStarting2Application.java
@@ -0,0 +1,13 @@
+package com.example.javastarting2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStarting2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStarting2Application.class, args);
+	}
+
+}
